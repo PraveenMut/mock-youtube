1b0b4ce645467f8b1c098d1df916528dd17de78e
praveen <praveen.mut@gmail.com>
2019-08-04 20:08:38 +1000

    create onInputChange function for text input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,16 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      term: '',
-    };
+    this.state = {}
 
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(e) {
-
+    const {currentTarget} = e;
+    this.setState({
+      [currentTarget.name]: currentTarget.value
+    });
   };
 
   render() {
@@ -22,6 +23,7 @@ class SearchBar extends Component {
           <div className="field">
             <label>Video Search</label>
             <input 
+              name="searchBar"
               type="text" 
               value={this.state.term}
               onChange={this.onInputChange}
