1e79dda03cefae9d923bab426799344789392233
praveen <praveen.mut@gmail.com>
2019-08-04 20:03:35 +1000

    Create input with props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,35 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input 
+              type="text" 
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
